8c2966d7c55e1c35f968f91aeb7c79cbdc8fb8d1
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-04 16:30:35 +0200

    Implemented living being's update method

--- src/artie_life/model/entities/entities.py
+++ src/artie_life/model/entities/entities.py
@@ -1,7 +1,10 @@
 """Module containing all entities implementations."""
 from typing import Tuple
 from numpy.random import uniform
+from numpy.random import randint
 from pygame import Rect
+from controller.world.world_controllers import ActionsController
+from utils import Action
 from utils import LIVING_WIDTH
 from utils import LIVING_HEIGHT
 
@@ -41,13 +44,37 @@ class InteractiveSpot(Entity):
 
 class LivingBeing(Entity):
     """Living being implementation, for characters."""
-    def __init__(self, hitbox: Rect) -> None:
+    def __init__(self, hitbox: Rect, controller: ActionsController) -> None:
         """Instantiates a living being.
         
         Arguments:  
         `hitbox`: the initial hitbox for the living being."""
         super().__init__(hitbox)
-        self.speed: float = 1.0
-        #self.genome = genome
-        #self.speed = genome.speed
-        #self.brain = Brain(genome, world)
+        self.controller = controller
+        self.speed: float = 0.2
+
+    def compute_movement(self, movement: float, elapsed_time: int) -> float:
+        return self.speed * elapsed_time * movement
+
+    def update(self, elapsed_time: int) -> None:
+        """Updates the living being, performing the desired action.
+        
+        Arguments:  
+        `elapsed_time`: the amount of time since last update."""
+        action_idx = randint(5)
+        action: Action = Action.INTERACT
+        for item in Action:
+            if item.value == action_idx:
+                action = item
+        
+        if action != Action.INTERACT:
+            move_x, move_y = action.get_direction()
+            moved_hitbox = self.hitbox.move(
+                self.compute_movement(move_x, elapsed_time),
+                self.compute_movement(move_y, elapsed_time)
+            )
+            if self.controller.can_move(moved_hitbox, id(self)):
+                self.hitbox = moved_hitbox
+        elif self.controller.can_interact(self.hitbox, id(self)):
+            # TODO - implement interaction
+            pass
