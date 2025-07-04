7dba4e400d180c80f161f0ac2d8fc5946ac02853
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-04 16:34:40 +0200

    Modified main module to request model update

--- src/artie_life/artie_life.py
+++ src/artie_life/artie_life.py
@@ -2,26 +2,32 @@
 
 if __name__=='__main__':
     import pygame
+    from pygame.time import Clock
     from view.game_view import GameView
     from controller.game_controller import GameController
 
     controller = GameController()
     controller.create_world()
     for _ in range(3):
-        controller.world.spawn_living()
+        controller.spawn_living()
 
     view = GameView()
 
     pygame.init()
+    clock = Clock()
 
     view.show_screen()
 
+    dt: int = 0
     running: bool = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        controller.update_world(dt)
         view.render(controller.get_map_elems())
+
+        dt = clock.tick(30)
 
     pygame.quit()
