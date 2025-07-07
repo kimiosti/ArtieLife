7e7939d8cb3848d68020ccfaab130b01fc02f39b
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-07 23:51:16 +0200

    Modified needs tracking and added distance controller

--- src/artie_life/model/entities/brain.py
+++ src/artie_life/model/entities/brain.py
@@ -1,20 +1,32 @@
 """Module containing the brain implementation."""
+from typing import TYPE_CHECKING
 from numpy.random import randint
 from utils import Action, InteractionType, BASE_HUNGER_DECAY, BASE_LIFE_DECAY, \
         BASE_TIREDNESS_DECAY, BASE_MATING_DRIVE_DECAY, BASE_HUNGER, BASE_LIFE, \
         BASE_TIREDNESS, BASE_MATING_DRIVE, MAX_HUNGER, MAX_TIREDNESS, MIN_LIFE, \
         BASE_DECISION_RATE
 
+if TYPE_CHECKING:
+    from typing import Dict
+    from controller.world.world_controllers import DistanceController
+
 class Brain:
     """Generic implementation for the living beings' brain."""
-    def __init__(self) -> "None":
-        """Instantiates the living being's brain."""
-        self.hunger: "float" = BASE_HUNGER
-        self.life: "float" = BASE_LIFE
-        self.tiredness: "float" = BASE_TIREDNESS
-        self.mating_drive: "float" = BASE_MATING_DRIVE
+    def __init__(self, distance_controller: "DistanceController", living_id: "int") -> "None":
+        """Instantiates the living being's brain.
+        
+        Arguments:  
+        `distance_controller`: the `DistanceController` tracking the living being's
+        perception of the world's space."""
+        self.needs: "Dict[str, float]" = {
+            "life": BASE_LIFE,
+            "hunger": BASE_HUNGER,
+            "tiredness": BASE_TIREDNESS,
+            "mating drive": BASE_MATING_DRIVE
+        }
         self.time_since_last_decision: "int" = 0
         self.action: "Action" = Action.INTERACT
+        self.controller: "DistanceController" = distance_controller
 
     def compute_new_action(self) -> "None":
         """Computes the next action to be performed."""
@@ -32,17 +44,17 @@ class Brain:
 
         Returns:  
         A `bool` representing whether the living being is still alive."""
-        self.hunger += elapsed_time * BASE_HUNGER_DECAY
-        self.tiredness += elapsed_time * BASE_TIREDNESS_DECAY
-        self.mating_drive += elapsed_time * BASE_MATING_DRIVE_DECAY
-        if self.hunger >= MAX_HUNGER and self.tiredness >= MAX_TIREDNESS:
-            self.life += BASE_LIFE_DECAY
+        self.needs["hunger"] += elapsed_time * BASE_HUNGER_DECAY
+        self.needs["tiredness"] += elapsed_time * BASE_TIREDNESS_DECAY
+        self.needs["mating drive"] += elapsed_time * BASE_MATING_DRIVE_DECAY
+        if self.needs["hunger"] >= MAX_HUNGER and self.needs["tiredness"] >= MAX_TIREDNESS:
+            self.needs["life"] += BASE_LIFE_DECAY
 
         self.time_since_last_decision += elapsed_time
         if self.time_since_last_decision >= BASE_DECISION_RATE:
             self.compute_new_action()
 
-        if self.life <= MIN_LIFE:
+        if self.needs["life"] <= MIN_LIFE:
             return False
         return True
 
@@ -60,10 +72,10 @@ class Brain:
         `interaction`: the type of the interaction to be made effective."""
         match interaction:
             case InteractionType.HEAL:
-                self.life = BASE_LIFE
+                self.needs["life"] = BASE_LIFE
             case InteractionType.FEED:
-                self.hunger = BASE_HUNGER
+                self.needs["hunger"] = BASE_HUNGER
             case InteractionType.REST:
-                self.tiredness = BASE_TIREDNESS
+                self.needs["tiredness"] = BASE_TIREDNESS
             case InteractionType.MATE:
-                self.mating_drive = BASE_MATING_DRIVE
+                self.needs["mating drive"] = BASE_MATING_DRIVE
