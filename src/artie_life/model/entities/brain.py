2b3eca8336e16ecfeb64b81afd4af72cfb98f0bf
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-08 00:44:30 +0200

    Added logging features to brain class

--- src/artie_life/model/entities/brain.py
+++ src/artie_life/model/entities/brain.py
@@ -1,6 +1,7 @@
 """Module containing the brain implementation."""
 from typing import TYPE_CHECKING
 from numpy.random import randint
+from controller.log import LivingLogger
 from utils import Action, InteractionType, BASE_HUNGER_DECAY, BASE_LIFE_DECAY, \
         BASE_TIREDNESS_DECAY, BASE_MATING_DRIVE_DECAY, BASE_HUNGER, BASE_LIFE, \
         BASE_TIREDNESS, BASE_MATING_DRIVE, MAX_HUNGER, MAX_TIREDNESS, MIN_LIFE, \
@@ -8,6 +9,7 @@ from utils import Action, InteractionType, BASE_HUNGER_DECAY, BASE_LIFE_DECAY, \
 
 if TYPE_CHECKING:
     from typing import Dict
+    from pygame.rect import Rect
     from controller.world.world_controllers import DistanceController
 
 class Brain:
@@ -27,20 +29,23 @@ class Brain:
         self.time_since_last_decision: "int" = 0
         self.action: "Action" = Action.INTERACT
         self.controller: "DistanceController" = distance_controller
+        self.logger: "LivingLogger" = LivingLogger(living_id)
 
-    def compute_new_action(self) -> "None":
+    def compute_new_action(self, hitbox: "Rect") -> "None":
         """Computes the next action to be performed."""
         action_idx = randint(5)
         for action in Action:
             if action.value == action_idx:
                 self.action = action
+        self.logger.dump(self.needs, self.controller.get_distance_by_type(hitbox), self.action)
         self.time_since_last_decision = 0
 
-    def update(self, elapsed_time: "int") -> "bool":
+    def update(self, elapsed_time: "int", hitbox: "Rect") -> "bool":
         """Updates the brain, decaying vital parameters.
 
         Arguments:  
-        `elapsed_time`: the amount of time since last brain update.
+        `elapsed_time`: the amount of time since last brain update.  
+        `hitbox`: the current position of the living being.
 
         Returns:  
         A `bool` representing whether the living being is still alive."""
@@ -52,9 +57,10 @@ class Brain:
 
         self.time_since_last_decision += elapsed_time
         if self.time_since_last_decision >= BASE_DECISION_RATE:
-            self.compute_new_action()
+            self.compute_new_action(hitbox)
 
         if self.needs["life"] <= MIN_LIFE:
+            self.logger.record_death()
             return False
         return True
 
