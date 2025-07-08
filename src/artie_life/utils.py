de21702cf9cb8c9381f543ce58640e1738a02c81
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-08 19:39:41 +0200

    Added needs enumerative class and interaction type utility method to retrieve the corresponding need, given an action

--- src/artie_life/utils.py
+++ src/artie_life/utils.py
@@ -31,16 +31,17 @@ LIVING_BASE_SPEED: "float" = 0.1
 
 # Living being vital parameters constants
 BASE_HUNGER: "float" = 0
-BASE_LIFE: "float" = 100
+BASE_LIFE: "float" = 0
 BASE_TIREDNESS: "float" = 0
 BASE_MATING_DRIVE: "float" = 0
 BASE_HUNGER_DECAY: "float" = 0.008
-BASE_LIFE_DECAY: "float" = -0.008
+BASE_LIFE_DECAY: "float" = 0.008
 BASE_TIREDNESS_DECAY: "float" = 0.008
 BASE_MATING_DRIVE_DECAY: "float" = 0.007
 MAX_HUNGER: "float" = 100
 MAX_TIREDNESS: "float" = 100
-MIN_LIFE: "float" = 0
+MAX_MATING_DRIVE: "float" = 100
+MAX_LIFE: "float" = 100
 BASE_DECISION_RATE: "int" = 100
 
 # Enumerative types
@@ -67,6 +68,66 @@ class Action(Enum):
         return (x, y)
 
 
+class Need(Enum):
+    """Enumerative class listing all living being needs."""
+    LIFE = 1
+    HUNGER = 2
+    TIREDNESS = 3
+    MATING_DRIVE = 0
+    NONE = 4
+
+    def get_base_value(self) -> "float":
+        """Getter for the need's base value.
+        
+        Returns:  
+        The need's base value as a `float`"""
+        match self:
+            case Need.MATING_DRIVE:
+                return BASE_MATING_DRIVE
+            case Need.LIFE:
+                return BASE_LIFE
+            case Need.HUNGER:
+                return BASE_HUNGER
+            case Need.TIREDNESS:
+                return BASE_TIREDNESS
+            case _:
+                return 0
+
+    def get_base_decay(self) -> "float":
+        """Getter for the base decay rate of a need.
+        
+        Returns:  
+        The need's base decay rate as a `float`."""
+        match self:
+            case Need.MATING_DRIVE:
+                return BASE_MATING_DRIVE_DECAY
+            case Need.LIFE:
+                return BASE_LIFE_DECAY
+            case Need.HUNGER:
+                return BASE_HUNGER_DECAY
+            case Need.TIREDNESS:
+                return BASE_TIREDNESS_DECAY
+            case _:
+                return 0
+
+    def get_threshold(self) -> "float":
+        """Getter for a need's limit value.
+        
+        Returns:  
+        The need's limit value as a `float`."""
+        match self:
+            case Need.MATING_DRIVE:
+                return MAX_MATING_DRIVE
+            case Need.LIFE:
+                return MAX_LIFE
+            case Need.HUNGER:
+                return MAX_HUNGER
+            case Need.TIREDNESS:
+                return MAX_TIREDNESS
+            case _:
+                return 0
+
+
 class InteractionType(Enum):
     """Enumerative class listing all possbile interaction results."""
     MATE = 0
@@ -74,6 +135,16 @@ class InteractionType(Enum):
     FEED = 2
     REST = 3
     NONE = 4
+
+    def get_corresponding_need(self) -> "Need":
+        """Translates an interaction type in the need that it fulfills.
+        
+        Returns:  
+        The desired `Need`."""
+        for need in Need:
+            if self.value == need.value:
+                return need
+        return Need.NONE
 
 
 class EntityType(Enum):
@@ -89,7 +160,10 @@ class EntityType(Enum):
         return self != EntityType.LIVING
 
     def get_interaction(self) -> "InteractionType":
-        """Check if a living being can interact with the entity type."""
+        """Translates an entity type into the corresponding interaction type.
+        
+        Returns:  
+        The desired `InteractionType`."""
         for interaction_type in InteractionType:
             if self.value == interaction_type.value:
                 return interaction_type
