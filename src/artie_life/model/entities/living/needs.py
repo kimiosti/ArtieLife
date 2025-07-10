7be880c519587b9bc9454118797b4740e7dc35f6
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-10 16:16:32 +0200

    Modified needs tracker and added perception tracker to keep and log current and average distance values from entity types

--- src/artie_life/model/entities/living/needs.py
+++ src/artie_life/model/entities/living/needs.py
@@ -1,15 +1,21 @@
 """Module containing the needs tracker's implementation."""
 from typing import TYPE_CHECKING
+from numpy import sqrt
 from controller.log import LivingLogger
-from utils import Need
+from utils import Need, EntityType
 
 if TYPE_CHECKING:
-    from typing import Dict
+    from typing import Dict, Tuple
+    from pygame.rect import Rect
+    from controller.world.world_controllers import DistanceController
 
 class NeedsTracker:
     """Implementation for the needs tracker of each living being."""
-    def __init__(self) -> "None":
-        """Instantiates a needs tracker."""
+    def __init__(self, living_id: "int") -> "None":
+        """Instantiates a needs tracker.
+        
+        Arguments:  
+        `living_id`: the living being's in-game ID."""
         self.needs: "Dict[Need, float]" = { }
         self.needs_avg: "Dict[Need, float]" = { }
         for need in Need:
@@ -17,6 +23,7 @@ class NeedsTracker:
                 self.needs[need] = need.get_base_value()
                 self.needs_avg[need] = 0
         self.observations: "int" = 0
+        self.logger: "LivingLogger" = LivingLogger(living_id)
 
     def decay(self, elapsed_time: "int") -> "bool":
         """Actuates a single decay step in all needs.
@@ -55,22 +62,53 @@ class NeedsTracker:
             self.needs_avg[need] = (self.needs_avg[need] * self.observations + value) \
                                    / (self.observations + 1)
         self.observations += 1
-
-    def get_avgs(self) -> "Dict[str, float]":
-        """Getter for the average needs values.
-        
-        Returns:  
-        A `Dict` containing all averages as `float` described by a label as `str`."""
-        return {
-            need.name.lower().replace("_", " "): value for need, value in self.needs_avg.items()
-        }
+        self.logger.dump_observation({
+            need.name.lower(): avg for need, avg in self.needs_avg.items()
+        })
 
     def get_needs(self) -> "Dict[str, float]":
-        """Getter for the last measured needs values.
+        """Getter for the current needs measure.
         
         Returns:  
-        A `Dict` containing all the last measured values as `float`
-        described by a label as a `str`."""
+        A `Dict` containing `float` values for each need, described by a `str` label."""
         return {
-            need.name.lower().replace("_", " "): value for need, value in self.needs.items()
+            need.name.lower(): value for need, value in self.needs.items()
         }
+
+
+class PerceptionTracker:
+    """Implementation for the living being's perception tracker."""
+    def __init__(self, controller: "DistanceController", living_id: "int") -> "None":
+        """Instantiates a perception tracker.
+        
+        Arguments:  
+        `controller`: the distance controller, responsible of calculating the
+        perceived values.  
+        `living_id`: the living being's in-game ID."""
+        self.perception: "Dict[EntityType, Tuple[float, float]]"
+        self.perception_avg: "Dict[EntityType, Tuple[float, float]]" = { }
+        for entity_type in EntityType:
+            self.perception_avg[entity_type] = (0, 0)
+        self.observations: "int" = 0
+        self.controller: "DistanceController" = controller
+        self.logger: "LivingLogger" = LivingLogger(living_id)
+
+    def record(self, hitbox: "Rect") -> "None":
+        """Records an observation of the environment.
+        
+        Arguments:  
+        `hitbox`: the living being's hitbox."""
+        self.perception = self.controller.get_distance_by_type(hitbox)
+        for entity_type, values in self.perception.items():
+            self.perception_avg[entity_type] = (
+                (self.perception_avg[entity_type][0] * self.observations + values[0])
+                    / (self.observations + 1),
+                (self.perception_avg[entity_type][1] * self.observations + values[1])
+                    / (self.observations + 1)
+            )
+        self.observations += 1
+        self.logger.dump_observation({
+            entity_type.name.lower():
+            sqrt(dists[0]**2 + dists[1]**2)
+            for entity_type, dists in self.perception.items()
+        })
