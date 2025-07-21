27befc3f492f6c7f34af117f3a2481fd0b24088e
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-21 20:32:50 +0200

    Modified main app module to accept text user input and user-driven rewards

--- src/artie_life/artie_life.py
+++ src/artie_life/artie_life.py
@@ -1,13 +1,16 @@
 """Main application module."""
 
 if __name__=='__main__':
-    import pygame
+    from pygame import QUIT, quit as quit_game, init as init_game
+    from pygame.event import get as get_all_events
     from pygame.time import Clock
+    from pygame.key import set_repeat as set_key_repeat
     from view.game_view import GameView
     from controller.game_controller import GameController
-    from controller.input import ClickController
+    from controller.input import ClickController, TextController
 
-    pygame.init()
+    init_game()
+    set_key_repeat(200, 75)
 
     game_controller: "GameController" = GameController()
     game_controller.create_world()
@@ -15,6 +18,7 @@ if __name__=='__main__':
     view: "GameView" = GameView()
 
     click_controller: "ClickController" = ClickController(game_controller.world, view)
+    text_controller: "TextController" = TextController(game_controller.world, view)
 
     clock: "Clock" = Clock()
 
@@ -23,23 +27,27 @@ if __name__=='__main__':
     dt: "int" = 0
     running: "bool" = True
     while running:
-        events = pygame.event.get()
+        events = get_all_events()
         for event in events:
-            if event.type == pygame.QUIT:
+            if event.type == QUIT:
                 running = False
 
-        click_controller.handle_living_selection(events)
         if click_controller.is_spawn_requested(events):
             game_controller.spawn_random_living()
 
         game_controller.update_world(dt / 1000)
         view.render(game_controller.get_map_elems())
 
+        click_controller.handle_living_selection(events)
         if game_controller.is_living_selected():
+            text_controller.update(events)
             view.render_bottom_bar(game_controller.get_selected_info())
+            click_controller.handle_user_reward(events)
+        else:
+            text_controller.clear()
 
         view.show_frame()
 
         dt = clock.tick(30)
 
-    pygame.quit()
+    quit_game()
