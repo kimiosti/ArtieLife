9c62c9c894315afc4f01ad1e68417f1bbb304704
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-22 10:18:29 +0200

    Added input actuation on the game's world

--- src/artie_life/controller/input.py
+++ src/artie_life/controller/input.py
@@ -2,7 +2,8 @@
 from typing import TYPE_CHECKING
 from pygame import MOUSEBUTTONDOWN, KEYDOWN, K_RETURN, K_BACKSPACE
 from pygame.mouse import get_pos as get_mouse_pos
-from utils.living.learning import MAX_INPUT_LENGTH
+from utils.living.learning.attention import MAX_INPUT_LENGTH, POSITIVE_REWARD, \
+        NEGATIVE_REWARD
 
 if TYPE_CHECKING:
     from typing import List
@@ -54,11 +55,10 @@ class ClickController:
         for event in events:
             if event.type == MOUSEBUTTONDOWN:
                 if self.view.bottom_bar.pos_reward.collidepoint(get_mouse_pos()):
-                    print("Positive reward applied")
-                    # TODO - apply positive reward
+                    self.world.apply_reward(POSITIVE_REWARD)
                 elif self.view.bottom_bar.neg_reward.collidepoint(get_mouse_pos()):
                     print("Negative reward applied")
-                    # TODO - apply negative reward
+                    self.world.apply_reward(NEGATIVE_REWARD)
 
 class TextController:
     """Implementation for the text input controller."""
@@ -85,7 +85,7 @@ class TextController:
                 if event.key == K_BACKSPACE:
                     self.view.bottom_bar.text = self.view.bottom_bar.text[:-1]
                 elif event.key == K_RETURN:
+                    self.world.send_input(self.view.bottom_bar.text)
                     self.clear()
-                    #TODO - implement input actuation
                 elif len(self.view.bottom_bar.text) < MAX_INPUT_LENGTH:
-                    self.view.bottom_bar.text += event.unicode
+                    self.view.bottom_bar.text += event.unicode.upper()
