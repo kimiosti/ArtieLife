131e67c2f2332dca0c05f6b901a9f8e87dd959c8
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-22 09:23:02 +0200

    Modified utils module structure for better organization

--- src/artie_life/utils/living/learning.py
+++
@@ -1,3 +0,0 @@
-"""Module containing learning utilities."""
-
-MAX_INPUT_LENGTH: "int" = 8
---
+++ src/artie_life/utils/living/learning/attention.py
@@ -0,0 +1,5 @@
+"""Module containing attention lobe utilities."""
+
+MAX_INPUT_LENGTH: "int" = 8
+POSITIVE_REWARD: "float" = 1
+NEGATIVE_REWARD: "float" = -1
