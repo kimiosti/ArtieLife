5feef2a1cb5b63025ec00d35d4cab7d7bd3091e6
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-23 12:28:26 +0200

    Refactored genome utility module to better handle gene admissible values

--- src/artie_life/utils/living/genome.py
+++ src/artie_life/utils/living/genome.py
@@ -1,5 +1,9 @@
 """Module containing utilities for genome."""
+from typing import TYPE_CHECKING
 from enum import Enum
+
+if TYPE_CHECKING:
+    from typing import Dict, Tuple
 
 MUTATION_RATE: "float" = 0.05
 
@@ -10,31 +14,28 @@ class Gene(Enum):
     TIREDNESS_DECAY = 2
     MATING_DRIVE_DECAY = 3
     SPEED = 4
+    ATTENTION_ALPHA = 5
+    ATTENTION_GAMMA = 6
+    ATTENTION_DECISION_PERIOD = 7
+    ATTENTION_TARGET_UPDATE_STEP = 8
 
     def min(self) -> "float":
         """Returns the minimum possible value for a given gene."""
-        match self:
-            case Gene.LIFE_DECAY:
-                return 2
-            case Gene.HUNGER_DECAY:
-                return 3
-            case Gene.TIREDNESS_DECAY:
-                return 3
-            case Gene.MATING_DRIVE_DECAY:
-                return 2
-            case Gene.SPEED:
-                return 45
+        return THRESHOLDS[self][0]
 
     def max(self) -> "float":
         """Returns the maximum possible value for a given gene."""
-        match self:
-            case Gene.LIFE_DECAY:
-                return 5
-            case Gene.HUNGER_DECAY:
-                return 7
-            case Gene.TIREDNESS_DECAY:
-                return 7
-            case Gene.MATING_DRIVE_DECAY:
-                return 5
-            case Gene.SPEED:
-                return 80
+        return THRESHOLDS[self][1]
+
+
+THRESHOLDS: "Dict[Gene, Tuple[float, float]]" = {
+    Gene.LIFE_DECAY: (2, 5),
+    Gene.HUNGER_DECAY: (3, 7),
+    Gene.TIREDNESS_DECAY: (3, 7),
+    Gene.MATING_DRIVE_DECAY: (2, 5),
+    Gene.SPEED: (45, 80),
+    Gene.ATTENTION_ALPHA: (1e-6, 1e-1),
+    Gene.ATTENTION_GAMMA: (1e-2, 1),
+    Gene.ATTENTION_DECISION_PERIOD: (1, 2),
+    Gene.ATTENTION_TARGET_UPDATE_STEP: (1, 1e2)
+}
