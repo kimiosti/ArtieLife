4f73208cbdd86989673aa42616e555aa502d81ab
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-29 18:12:35 +0200

    Added utility constants to model different kind of rewards

--- src/artie_life/utils/living/learning/attention.py
+++ src/artie_life/utils/living/learning/attention.py
@@ -4,12 +4,16 @@ from keras import layers
 from utils.living.actions import EntityType
 
 MAX_INPUT_LENGTH: "int" = 8
-POSITIVE_REWARD: "float" = 1
-NEGATIVE_REWARD: "float" = -1
+POSITIVE_USER_REWARD: "float" = 1
+NEGATIVE_USER_REWARD: "float" = -1
+POSITIVE_DISTANCE_REWARD: "float" = 1
+NEGATIVE_DISTANCE_REWARD: "float" = -1
+POSITIVE_NEEDS_REWARD: "float" = 1
+NEGATIVE_NEEDS_REWARD: "float" = 0
 INPUT_LAYER_DIM: "int" = 2 * (len(EntityType) - 1) + MAX_INPUT_LENGTH
 OUTPUT_LAYER_DIM: "int" = len(EntityType) - 2
 
-def create_attention_model() -> "Sequential":
+def create_attention_model() -> "Sequential": # type: ignore[no-any-unimported]
     """Instantiates the model underlying the Attention lobe."""
     return Sequential([
         layers.Input(shape=(INPUT_LAYER_DIM,)),
