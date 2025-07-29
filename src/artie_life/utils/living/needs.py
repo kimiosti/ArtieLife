01055b2a213c228a9bf6e55476e00f8bfad7591a
Kimi Osti <kimi.osti@studio.unibo.it>
2025-07-29 16:08:37 +0200

    Removed mating drive

--- src/artie_life/utils/living/needs.py
+++ src/artie_life/utils/living/needs.py
@@ -5,19 +5,16 @@ from utils.living.genome import Gene
 BASE_HUNGER: "float" = 0
 BASE_LIFE: "float" = 0
 BASE_TIREDNESS: "float" = 0
-BASE_MATING_DRIVE: "float" = 0
 MAX_HUNGER: "float" = 100
 MAX_TIREDNESS: "float" = 100
-MAX_MATING_DRIVE: "float" = 100
 MAX_LIFE: "float" = 100
 
 class Need(Enum):
     """Enumerative class listing all living being needs."""
-    LIFE = 1
-    HUNGER = 2
-    TIREDNESS = 3
-    MATING_DRIVE = 0
-    NONE = 4
+    LIFE = 0
+    HUNGER = 1
+    TIREDNESS = 2
+    NONE = 3
 
     def get_base_value(self) -> "float":
         """Getter for the need's base value.
@@ -25,8 +22,6 @@ class Need(Enum):
         Returns:  
         The need's base value as a `float`"""
         match self:
-            case Need.MATING_DRIVE:
-                return BASE_MATING_DRIVE
             case Need.LIFE:
                 return BASE_LIFE
             case Need.HUNGER:
@@ -42,8 +37,6 @@ class Need(Enum):
         Returns:  
         The gene representing the need's decay rate."""
         match self:
-            case Need.MATING_DRIVE:
-                return Gene.MATING_DRIVE_DECAY
             case Need.LIFE:
                 return Gene.LIFE_DECAY
             case Need.HUNGER:
@@ -59,8 +52,6 @@ class Need(Enum):
         Returns:  
         The need's limit value as a `float`."""
         match self:
-            case Need.MATING_DRIVE:
-                return MAX_MATING_DRIVE
             case Need.LIFE:
                 return MAX_LIFE
             case Need.HUNGER:
