1e1e770b1f9dd30f5e7850c67f1630710d880797
Kimi Osti <kimi.osti@studio.unibo.it>
2025-10-02 19:38:09 +0200

    Modified brain coordination lobe to properly handle learning enable argument option

--- src/artie_life/model/entities/living/brain/central.py
+++ src/artie_life/model/entities/living/brain/central.py
@@ -1,9 +1,8 @@
 """Module containing the brain implementation."""
 from typing import TYPE_CHECKING
-from numpy.linalg import norm
 from model.entities.living.needs import NeedsTracker, PerceptionTracker
-from model.entities.living.brain.attention import Attention
-from model.entities.living.brain.reason import Reason
+from model.entities.living.brain.attention import Attention, LearningAttention
+from model.entities.living.brain.reason import Reason, LearningReason
 from controller.log import log_genome
 
 if TYPE_CHECKING:
@@ -20,45 +19,43 @@ class Brain:
         """Instantiates the living being's brain.
         
         Positional arguments:  
-        `distance_controller`: the `DistanceController` tracking the living being's
+         - `distance_controller`: the `DistanceController` tracking the living being's \
         perception of the world's space.
-        `living_id`: the living being's in-game ID.
-        `genome`: the living being's genome.  
-        `learning_enable`: a `bool` representing if the living being should learn  
+         - `living_id`: the living being's in-game ID.
+         - `genome`: the living being's genome.
+         - `learning_enable`: a `bool` representing if the living being should learn \
         or act randomly."""
-        # TODO - implement learning enable actuation in brain lobes instantiation.
         log_genome(living_id, genome)
         self.perception_tracker = PerceptionTracker(distance_controller)
         self.needs_tracker = NeedsTracker(genome)
-        self.reason: "Reason" = Reason(genome, living_id)
-        self.attention: "Attention" = Attention(genome, living_id)
+        self.attention: "Attention" = \
+            LearningAttention(genome) if learning_enable else Attention(genome)
+        self.reason: "Reason" = \
+            LearningReason(genome) if learning_enable else Reason(genome)
 
     def update(self, elapsed_time: "float", hitbox: "Rect") -> "bool":
         """Updates the brain, decaying vital parameters.
 
-        Arguments:  
-        `elapsed_time`: the amount of time since last brain update, in seconds.  
-        `hitbox`: the current position of the living being.
+        Positional arguments:  
+         - `elapsed_time`: the amount of time since last brain update, in seconds.
+         - `hitbox`: the current position of the living being.
 
-        Returns:  
+        Return:  
         A `bool` representing whether the living being is still alive."""
+
+        # pylint: disable=locally-disabled, unidiomatic-typecheck
+        # Warnings disabled because the behavior of type is the desired behavior, since it allows
+        # to distinguish the subclass from the superclass.
+
         self.perception_tracker.record(hitbox)
-        self.attention.update(
-            elapsed_time,
-            self.needs_tracker.needs,
-            self.perception_tracker.perception
-        )
-        self.reason.update(
-            self.attention.focus,
-            self.needs_tracker.needs,
-            self.needs_tracker.fitness,
-            {
-                entity_type: norm(value)
-                for entity_type, value
-                in self.perception_tracker.perception.items()
-            },
-            elapsed_time
-        )
+        if type(self.attention) is Attention:
+            self.attention.update()
+        else:
+            self.attention.update() # TODO - add relevant arguments
+        if type(self.attention) is Reason:
+            self.reason.update()
+        else:
+            self.reason.update() # TODO - add relevant arguments
         return self.needs_tracker.decay(elapsed_time)
 
     def actuate(self, interaction: "InteractionType") -> "None":
