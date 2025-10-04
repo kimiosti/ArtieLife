af6a5af11c94d3881dec90f3b3d202674f0cc646
Kimi Osti <kimi.osti@studio.unibo.it>
2025-10-04 12:52:04 +0200

    Implemented self-determined reward computation and user-defined reward and input application. Also added first-frame random action guard to ensure that all perceptions are relevant while calling learning algorithms

--- src/artie_life/model/entities/living/brain/central.py
+++ src/artie_life/model/entities/living/brain/central.py
@@ -1,20 +1,61 @@
 """Module containing the brain implementation."""
 from typing import TYPE_CHECKING
+from numpy.linalg import norm
+from utils.living.learning.commons import USER_INTERACTION_PERIOD, POSITIVE_MOVEMENT_REWARD, \
+    NEGATIVE_MOVEMENT_REWARD, POSITIVE_NEEDS_REWARD, NEGATIVE_NEEDS_REWARD
+from utils.living.actions import Action
 from model.entities.living.needs import NeedsTracker, PerceptionTracker
 from model.entities.living.brain.attention import Attention, LearningAttention
 from model.entities.living.brain.reason import Reason, LearningReason
 from controller.log import log_genome
 
 if TYPE_CHECKING:
-    from typing import Dict
+    from typing import Dict, Tuple
     from pygame.rect import Rect
     from controller.world.world_controllers import DistanceController
     from utils.living.genome import Gene
     from utils.living.actions import InteractionType
+    from utils.living.actions import Need
+
+def compute_movement_reward(last_distance: "Tuple[float, float]",
+                            cur_distance: "Tuple[float, float]") -> "float":
+    """Computes the auto-determined reward component related to movement.  
+    
+    Positional arguments:  
+     - `last_distance`: the last previously recorded distance from the closest object \
+    of the same type as the current living being focus.
+     - `cur_distance`: the current distance from the closest object of the same type as \
+    the current living being focus.
+    
+    Return:  
+    The computed reward value."""
+    return POSITIVE_MOVEMENT_REWARD if norm(cur_distance) < norm(last_distance) \
+        else NEGATIVE_MOVEMENT_REWARD
+
+def compute_needs_reward(last_needs: "Dict[Need, float]",
+                         cur_needs: "Dict[Need, float]") -> "float":
+    """Computes the auto-determined reward component related to needs' decay or
+    restoration.
+    
+    Positional arguments:  
+     - `last_needs`: the last previously recorded value for each need.
+     - `cur_needs`: the current value for each need.
+    
+    Return:  
+    The computed reward value."""
+    for need, value in cur_needs.items():
+        if value <= last_needs[need] and value < need.get_threshold():
+            return POSITIVE_NEEDS_REWARD
+    return NEGATIVE_NEEDS_REWARD
 
 class Brain:
     """Implementation for the living being's central brain lobe. Its function is to
     coordinate the behavior of the `Attention` and `Reason` lobes."""
+
+    # pylint: disable=too-many-instance-attributes
+    # Warning disabled since all attributes are needed, and are used to avoid
+    # larger issues in the single learning lobes.
+
     def __init__(self, distance_controller: "DistanceController", living_id: "int",
                  genome: "Dict[Gene, float]", learning_enable: "bool") -> "None":
         """Instantiates the living being's central lobe.
@@ -33,6 +74,10 @@ class Brain:
             LearningAttention(genome) if learning_enable else Attention(genome)
         self.reason: "Reason" = \
             LearningReason(genome) if learning_enable else Reason(genome)
+        self.user_reward: "float" = 0.0
+        self.user_input: "str" = ""
+        self.user_interaction_length: "float" = 0.0
+        self.first_frame = True
 
     def update(self, elapsed_time: "float", hitbox: "Rect") -> "bool":
         """Updates the brain, decaying vital parameters.
@@ -43,21 +88,48 @@ class Brain:
 
         Return:  
         `True` if the living being is still alive after the update, `False` otherwise."""
+        if self.first_frame:
+            self.attention.update()
+            self.reason.update()
+            self.first_frame = False
+            self.perception_tracker.record(hitbox)
+            return self.needs_tracker.decay(elapsed_time)
 
-        # pylint: disable=locally-disabled, unidiomatic-typecheck
-        # Warnings disabled because the behavior of type is the desired behavior, since it allows
-        # to distinguish the subclass from the superclass.
+        self.user_interaction_length += elapsed_time
+        if self.user_interaction_length > USER_INTERACTION_PERIOD:
+            self.user_input = ""
+            self.user_reward = 0.0
+            self.user_interaction_length = 0.0
+
+        environmental_reward: "float" = 0.0
+        last_perception = self.perception_tracker.perception
+        last_needs = self.needs_tracker.needs
 
         self.perception_tracker.record(hitbox)
-        if type(self.attention) is Attention:
+        is_alive = self.needs_tracker.decay(elapsed_time)
+
+        if not is_alive:
+            return is_alive
+
+        if self.reason.action != Action.INTERACT:
+            environmental_reward += compute_movement_reward(
+                last_perception[self.attention.focus],
+                self.perception_tracker.perception[self.attention.focus]
+            )
+        environmental_reward += compute_needs_reward(
+            last_needs,
+            self.needs_tracker.needs
+        )
+
+        if isinstance(self.attention, LearningAttention) \
+                and isinstance(self.reason, LearningReason):
+            # TODO - implement learning algo call
+            pass
+        else:
             self.attention.update()
-        else:
-            self.attention.update() # TODO - add relevant arguments
-        if type(self.attention) is Reason:
             self.reason.update()
-        else:
-            self.reason.update() # TODO - add relevant arguments
-        return self.needs_tracker.decay(elapsed_time)
+
+        return is_alive
 
     def actuate(self, interaction: "InteractionType") -> "None":
         """Actuates the effect of a given interaction on the living being's needs.
@@ -65,3 +137,18 @@ class Brain:
         Positional arguments:  
          - `interaction`: the type of the interaction to be made effective."""
         self.needs_tracker.actuate(interaction.get_corresponding_need())
+
+    def apply_user_reward(self, reward: "float") -> "None":
+        """Applies the desired user-defined reward to the living being.
+        
+        Positional arguments:  
+         - `reward`: the value of the user-defined reward to be applied to the \
+        living being."""
+        self.user_reward = reward
+
+    def record_input(self, input_text: "str") -> "None":
+        """Records the desired user input.
+        
+        Positional arguments:  
+         - `input_text`: the user-defined input string."""
+        self.user_input = input_text
